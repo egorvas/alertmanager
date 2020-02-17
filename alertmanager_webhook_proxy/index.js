75935664c88dc8c5aa1cd5ce38fde89bb52c7869
Egor Vasilyev <egorvas@gmail.com>
2020-02-17 14:36:32 +0700

    Use reason instead of severity in alertmanager

--- alertmanager_webhook_proxy/index.js
+++ alertmanager_webhook_proxy/index.js
@@ -42,11 +42,12 @@ function forwardWebhook(req){
 }
 
 function postAlert(req, isFinish){
-    request.get(ALERT_MANAGER_URL+"/api/v2/alerts", {qs: "active=true&filter={alertname=\""+req.body.name+"\",severity=\""+req.body.severity+"\",instance=\""+req.body.severity+"\"}"}, function (error, response) {
+    if (typeof req.body.reason === 'undefined' || req.body.reason) req.body.reason = "Alert "+ req.body.name + " failed";
+    request.get(ALERT_MANAGER_URL+"/api/v2/alerts", {qs: "active=true&filter={alertname=\""+req.body.name+"\",reason=\""+req.body.reason+"\",instance=\""+req.body.severity+"\"}"}, function (error, response) {
         if (error) logger.log('error', error);
         const responseAlertmanager = JSON.parse(response.body);
         let data = [{
-                labels: {alertname: req.body.name, severity: req.body.severity, instance: req.body.instance},
+                labels: {alertname: req.body.name, reason: req.body.reason, instance: req.body.instance},
                 annotations: {message: req.body.message},
                 generatorURL: req.body.url}];
 
